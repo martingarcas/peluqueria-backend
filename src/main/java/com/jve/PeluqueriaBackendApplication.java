d351ccf0320efbad5d6a9c523afdded6c31eab95
Martín García Castillo <mgarcas320@g.educaand.es>
2025-04-08 17:33:09 +0200

    First commit

---
+++ src/main/java/com/jve/PeluqueriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaBackendApplication.class, args);
+	}
+
+}
